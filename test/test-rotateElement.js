ec4a4f3b09c1f6246c4debf6846b7a4d41007fb3
Pavel Beshchuk <PashaBeshchuk1990@gmail.com>
2019-05-20 22:57:23 +0400

    edited tests file and edited tetris.js

--- test/test-rotateElement.js
+++ test/test-rotateElement.js
@@ -23,8 +23,8 @@ describe("Rotation where afte element has positive,negative, top, off the diagon
         `)
 
         let stepElementTetromino = tetris.rotateElement(elementTetromino[0], pivot[0])
-        let expectedResult = [{ x: -2, y: 2 }]
-        assert.deepEqual(expectedResult[0], stepElementTetromino)
+        let expectedResult = { x: -2, y: 2 }
+        assert.deepEqual(expectedResult, stepElementTetromino)
     })
 
     it("element move the bottom field 4x7", function () {
@@ -121,8 +121,9 @@ describe("Rotation where afte element has positive,negative, top, off the diagon
         assert.deepEqual(expectedResult[0], stepElementTetromino)
     })
 })
+
 describe("Rotation where starts to the left,on the diagonal, has negative coordinates", function () {
-    it("element move the right on the diagonal field 3x5 ", function () {
+    it("element move the right on the diagonal field 3x5", function () {
         let elementTetromino = tetris.getCoordinates(`
             -----
             -----
@@ -139,9 +140,11 @@ describe("Rotation where starts to the left,on the diagonal, has negative coordi
             -----
             ----X
         `)
+        assert.deepEqual(expectedResult[0], stepElementTetromino)
+
     })
 
-    it("element move the top on the diagonal field 3x5 ", function () {
+    it("element move the top on the diagonal field 3x5", function () {
         let elementTetromino = tetris.getCoordinates(`
             -----
             -----
@@ -153,37 +156,41 @@ describe("Rotation where starts to the left,on the diagonal, has negative coordi
             -----
         `)
         let stepElementTetromino = tetris.rotateElement(elementTetromino[0], pivot[0])
-        let expectedResult = [{ x: 4, y: -2 }]
+        let expectedResult = { x: 4, y: -2 }
+        assert.deepEqual(expectedResult, stepElementTetromino)
     })
 
-    it("element move the left on the diagonal field 3x5 ", function () {
-        let elementTetromino = [{ x: 4, y: -2 }]
+    it("element move the left on the diagonal field 3x5", function () {
+        let elementTetromino = { x: 4, y: -2 }
         let pivot = tetris.getCoordinates(`
             --X--
             -----
             -----
         `)
-        let stepElementTetromino = tetris.rotateElement(elementTetromino[0], pivot[0])
-        let expectedResult = [{ x: 0, y: -2 }]
+        let stepElementTetromino = tetris.rotateElement(elementTetromino, pivot[0])
+        let expectedResult = { x: 0, y: -2 }
+        assert.deepEqual(expectedResult, stepElementTetromino)
     })
 
-    it("element move the bottom on the diagonal field 3x5 ", function () {
-        let elementTetromino = [{ x: 0, y: -2 }]
+    it("element move the bottom on the diagonal field 3x5", function () {
+        let elementTetromino = { x: 0, y: -2 }
         let pivot = tetris.getCoordinates(`
             --X--
             -----
             -----
         `)
-        let stepElementTetromino = tetris.rotateElement(elementTetromino[0], pivot[0])
+        let stepElementTetromino = tetris.rotateElement(elementTetromino, pivot[0])
         let expectedResult = tetris.getCoordinates(`
             -----
             -----
             X----
         `)
+        assert.deepEqual(expectedResult[0], stepElementTetromino)
     })
 })
+
 describe("Rotation where starts to the right, pivot has same x and y coordinates", function () {
-    it("element move the top field 5x5 ", function () {
+    it("element move the top field 5x5", function () {
         let elementTetromino = tetris.getCoordinates(`
             -----
             ----X
@@ -207,9 +214,10 @@ describe("Rotation where starts to the right, pivot has same x and y coordinates
             -----
             -----
         `)
+        assert.deepEqual(expectedResult[0], stepElementTetromino)
     })
 
-    it("element move the left field 5x5 ", function () {
+    it("element move the left field 5x5", function () {
         let elementTetromino = tetris.getCoordinates(`
             -X---
             -----
@@ -233,9 +241,10 @@ describe("Rotation where starts to the right, pivot has same x and y coordinates
             X----
             -----
         `)
+        assert.deepEqual(expectedResult[0], stepElementTetromino)
     })
 
-    it("element move the bottom field 5x5 ", function () {
+    it("element move the bottom field 5x5", function () {
         let elementTetromino = tetris.getCoordinates(`
             -----
             -----
@@ -259,9 +268,10 @@ describe("Rotation where starts to the right, pivot has same x and y coordinates
             -----
             ---X-
         `)
+        assert.deepEqual(expectedResult[0], stepElementTetromino)
     })
 
-    it("element move the right field 5x5 ", function () {
+    it("element move the right field 5x5", function () {
         let elementTetromino = tetris.getCoordinates(`
             -----
             -----
@@ -285,88 +295,101 @@ describe("Rotation where starts to the right, pivot has same x and y coordinates
             -----
             -----
         `)
+        assert.deepEqual(expectedResult[0], stepElementTetromino)
     })
 })
+
 describe("Rotation where starts to the bottom, pivot has different x and y coordinates", function () {
-    it("element move the left field 6x5 ", function () {
+    it("element move the left field 6x6", function () {
         let elementTetromino = tetris.getCoordinates(`
-            -----
-            -----
-            -----
-            -----
-            -----
-            -X---
+            ------
+            ------
+            ------
+            ------
+            ------
+            --X---
         `)
         let pivot = tetris.getCoordinates(`
-            -----
-            -----
-            -X---
-            -----
-            -----
-            -----
+            ------
+            ------
+            --X---
+            ------
+            ------
+            ------
         `)
         let stepElementTetromino = tetris.rotateElement(elementTetromino[0], pivot[0])
         let expectedResult = tetris.getCoordinates(`
-            -----
-            -----
-            ----X
-            -----
-            -----
-            -----
+            ------
+            -----X
+            ------
+            ------
+            ------
+            ------
         `)
+        assert.deepEqual(expectedResult[0], stepElementTetromino)
     })
 
-    it("element move the top field 6x5 ", function () {
+    it("element move the top field 6x6", function () {
         let elementTetromino = tetris.getCoordinates(`
-            -----
-            -----
-            ----X
-            -----
-            -----
-            -----
+            ------
+            -----X
+            ------
+            ------
+            ------
+            ------
         `)
         let pivot = tetris.getCoordinates(`
-            -----
-            -----
-            -X---
-            -----
-            -----
-            -----
+            ------
+            ------
+            --X---
+            ------
+            ------
+            ------
         `)
         let stepElementTetromino = tetris.rotateElement(elementTetromino[0], pivot[0])
-        let expectedResult = [{ x: 1, y: -1 }]
+        let expectedResult = { x: 1, y: -1 }
+        assert.deepEqual(expectedResult, stepElementTetromino)
     })
 
-    it("element move the top field 6x5 ", function () {
-        let elementTetromino = [{ x: 1, y: -1 }]
+    it("element move the top field 6x6", function () {
+        let elementTetromino = { x: 1, y: -1 }
         let pivot = tetris.getCoordinates(`
-            -----
-            -----
-            -X---
-            -----
-            -----
-            -----
+            ------
+            ------
+            --X---
+            ------
+            ------
+            ------
         `)
-        let stepElementTetromino = tetris.rotateElement(elementTetromino[0], pivot[0])
-        let expectedResult = [{ x: 1, y: -1 }]
+        let stepElementTetromino = tetris.rotateElement(elementTetromino, pivot[0])
+        let expectedResult = { x: -1, y: 3 }
+        assert.deepEqual(expectedResult, stepElementTetromino)
     })
-
-    it("element move the top field 6x5 ", function () {
-        let elementTetromino = [{ x: 1, y: -1 }]
+    it("element move the top field 6x6", function () {
+        let elementTetromino = { x: -1, y: 3 }
         let pivot = tetris.getCoordinates(`
-            -----
-            -----
-            -X---
-            -----
-            -----
-            -----
+            ------
+            ------
+            --X---
+            ------
+            ------
+            ------
         `)
-        let stepElementTetromino = tetris.rotateElement(elementTetromino[0], pivot[0])
-        let expectedResult = [{ x: -2, y: 2 }]
+        let stepElementTetromino = tetris.rotateElement(elementTetromino, pivot[0])
+        let expectedResult = tetris.getCoordinates(`
+            ------
+            ------
+            ------
+            ------
+            ------
+            --X---
+        `)
+        assert.deepEqual(expectedResult, stepElementTetromino)
     })
 })
+
 describe("Rotation where pivot coordinates coincides with element coordinates", function () {
-    it("element not move field 4x4 ", function () {
+    it("element not move field 4x4", function () {
         let elementTetromino = tetris.getCoordinates(`
            ----
            ----
@@ -386,5 +409,6 @@ describe("Rotation where pivot coordinates coincides with element coordinates", 
             -X--
             ----
         `)
+        assert.deepEqual(expectedResult[0], stepElementTetromino)
     })
 })
