3f4d7c95620bdb8f853b569b88684e4e4b61a5bc
Pavel Beshchuk <PashaBeshchuk1990@gmail.com>
2019-05-23 21:34:38 +0400

    edit function

--- tetris.js
+++ tetris.js
@@ -305,11 +305,8 @@ module.exports = {
 		return arrayOfCoordinates;
 	},
 	rotateElement: function(coordinatesElement, coordinatesPivot){
-		let internalVariable = {};
-		let numb  = coordinatesPivot.y - coordinatesElement.y
-		let numbr = coordinatesPivot.x - coordinatesElement.x
-		internalVariable.x = coordinatesPivot.x - numb  
-		internalVariable.y = coordinatesPivot.y + numbr
-		return internalVariable
+		let coordinateDifference_X  = coordinatesPivot.y - coordinatesElement.y
+		let coordinateDifference_Y = coordinatesPivot.x - coordinatesElement.x
+		return {x:coordinatesPivot.x - coordinateDifference_X, y:coordinatesPivot.y + coordinateDifference_Y}
 	}
 }
