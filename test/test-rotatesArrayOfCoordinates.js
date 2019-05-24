bbd8340001e70750da49f26dc3326437f22d6bcc
Pavel Beshchuk <PashaBeshchuk1990@gmail.com>
2019-05-24 20:03:57 +0400

    all test readi

--- test/test-rotatesArrayOfCoordinates.js
+++ test/test-rotatesArrayOfCoordinates.js
@@ -19,7 +19,7 @@ describe("Incoming array is empty", function () {
 });
 
 describe("Incoming array has 1 element", function () {
-    it("Next coordinates will be beyond the field", function () {
+    it("Next coordinate will be beyond the field", function () {
         let arrayOfCoordinates = tetris.getCoordinates(`
             -X--
             ----
@@ -40,7 +40,7 @@ describe("Incoming array has 1 element", function () {
         assert.deepEqual(stepElementTetromino, expectedResult);
     });
 
-    it("Coordinates move down form pivot", function () {
+    it("Coordinate move down form pivot", function () {
         let arrayOfCoordinates = [{ x: -1, y: 2 }];
         let pivot = tetris.getCoordinates(`
             ----
@@ -60,7 +60,7 @@ describe("Incoming array has 1 element", function () {
         assert.deepEqual(stepElementTetromino, expectedResult);
     });
 
-    it("Coordinates move left form pivot", function () {
+    it("Coordinate move left form pivot", function () {
         let arrayOfCoordinates = tetris.getCoordinates(`
             ----
             ----
@@ -87,7 +87,7 @@ describe("Incoming array has 1 element", function () {
         assert.deepEqual(stepElementTetromino, expectedResult);
     });
 
-    it("Coordinates move top form pivot", function (){
+    it("Coordinate move top form pivot", function () {
         let arrayOfCoordinates = tetris.getCoordinates(`
             ----
             ----
@@ -112,5 +112,223 @@ describe("Incoming array has 1 element", function () {
             ----
         `);
         assert.deepEqual(stepElementTetromino, expectedResult);
-    })
+    });
 });
+
+describe("Incoming array has more than 1 element and that are next to each other", function () {
+    it("Next coordinates will be beyond the field", function () {
+        let arrayOfCoordinates = tetris.getCoordinates(`
+            -XX-
+            ----
+            ----
+            ----
+            ----
+        `);
+
+        let pivot = tetris.getCoordinates(`
+            ----
+            ----
+            -X--
+            ----
+            ----
+        `);
+        let stepArrayCoordinates = tetris.rotatesArrayOfCoordinates(arrayOfCoordinates, pivot);
+        let expectedResult = [{ x: -1, y: 1 }, { x: -1, y: 2 }];
+        assert.deepEqual(stepElementTetromino, expectedResult);
+    });
+
+    it("Coordinates move down form pivot", function () {
+        let arrayOfCoordinates = [{ x: -1, y: 1 }, { x: -1, y: 2 }];
+        let pivot = tetris.getCoordinates(`
+            ----
+            ----
+            -X--
+            ----
+            ----
+        `);
+        let stepArrayCoordinates = tetris.rotatesArrayOfCoordinates(arrayOfCoordinates, pivot);
+        let expectedResult = tetris.getCoordinates(`
+            ----
+            ----
+            ----
+            ----
+            XX--
+        `);
+        assert.deepEqual(stepElementTetromino, expectedResult);
+    });
+
+    it("Coordinates move left form pivot", function () {
+        let arrayOfCoordinates = tetris.getCoordinates(`
+            ----
+            ----
+            ----
+            ----
+            XX--
+        `);
+
+        let pivot = tetris.getCoordinates(`
+            ----
+            ----
+            -X--
+            ----
+            ----
+        `);
+        let stepArrayCoordinates = tetris.rotatesArrayOfCoordinates(arrayOfCoordinates, pivot);
+        let expectedResult = tetris.getCoordinates(`
+            ----
+            ----
+            ---X
+            ---X
+            ----
+        `);
+        assert.deepEqual(stepElementTetromino, expectedResult);
+    });
+
+    it("Coordinates move top form pivot", function () {
+        let arrayOfCoordinates = tetris.getCoordinates(`
+            ----
+            ----
+            ---X
+            ---X
+            ----
+        `);
+
+        let pivot = tetris.getCoordinates(`
+            ----
+            ----
+            -X--
+            ----
+            ----
+        `);
+        let stepArrayCoordinates = tetris.rotatesArrayOfCoordinates(arrayOfCoordinates, pivot);
+        let expectedResult = tetris.getCoordinates(`
+            -XX-
+            ----
+            ----
+            ----
+            ----
+        `);
+        assert.deepEqual(stepElementTetromino, expectedResult);
+    });
+});
+
+describe("Incoming array has more than 1 element and that are next to each other", function () {
+    it("Coordinates move left form pivot", function () {
+        let arrayOfCoordinates = tetris.getCoordinates(`
+            -------
+            -------
+            -------
+            -------
+            -----X-
+            ---XX--
+        `);
+
+        let pivot = tetris.getCoordinates(`
+            -------
+            -------
+            -------
+            ---X---
+            -------
+            -------
+            
+        `);
+        let stepArrayCoordinates = tetris.rotatesArrayOfCoordinates(arrayOfCoordinates, pivot);
+        let expectedResult = tetris.getCoordinates(`
+            -------
+            ----X--
+            -----X-
+            -----X-
+            -------
+            -------
+        `);
+        assert.deepEqual(stepElementTetromino, expectedResult);
+    });
+
+    it("Coordinate move top form pivot", function () {
+        let arrayOfCoordinates = tetris.getCoordinates(`
+            -------
+            ----X--
+            -----X-
+            -----X-
+            -------
+            -------
+        `);
+
+        let pivot = tetris.getCoordinates(`
+            -------
+            -------
+            -------
+            ---X---
+            -------
+            -------
+            
+        `);
+        let stepArrayCoordinates = tetris.rotatesArrayOfCoordinates(arrayOfCoordinates, pivot);
+        let expectedResult = tetris.getCoordinates(`
+            -------
+            --XX---
+            -X-----
+            -------
+            -------
+            -------
+        `);
+    });
+    it("Coordinate move right form pivot", function () {
+        let arrayOfCoordinates = tetris.getCoordinates(`
+            -------
+            --XX---
+            -X-----
+            -------
+            -------
+            -------
+        `);
+
+        let pivot = tetris.getCoordinates(`
+            -------
+            -------
+            -------
+            ---X---
+            -------
+            -------
+            
+        `);
+        let stepArrayCoordinates = tetris.rotatesArrayOfCoordinates(arrayOfCoordinates, pivot);
+        let expectedResult = tetris.getCoordinates(`
+            -------
+            -------
+            -------
+            -X-----
+            -X-----
+            --X----
+        `);
+    });
+    it("Coordinate move down form pivot", function () {
+        let arrayOfCoordinates = tetris.getCoordinates(`
+            -------
+            -------
+            -------
+            -X-----
+            -X-----
+            --X----
+        `);
+
+        let pivot = tetris.getCoordinates(`
+            -------
+            -------
+            -------
+            ---X---
+            -------
+            -------
+            
+        `);
+        let stepArrayCoordinates = tetris.rotatesArrayOfCoordinates(arrayOfCoordinates, pivot);
+        let expectedResult = tetris.getCoordinates(`
+            -------
+            -------
+            -------
+            -------
+            -----X-
+            ---XX--
+        `);
+    });
+})
