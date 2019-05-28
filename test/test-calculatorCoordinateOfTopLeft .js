c877060c74c9d58a05200079469e9f04c7004a1b
Pavel Beshchuk <PashaBeshchuk1990@gmail.com>
2019-05-28 23:32:50 +0400

    add tests

--- test/test-calculatorCoordinateOfTopLeft .js
+++ test/test-calculatorCoordinateOfTopLeft .js
@@ -1,2 +1,81 @@
 var assert = require('chai').assert;
 var tetris = require('../tetris')
+describe("Coordinates next to each other and arrays is many elements, matches coordinate of one of the elements", function(){
+    it("Arrays is many elements", function(){
+        arrayOfCoordinates = tetris.getCoordinates(`
+            -----
+            --XX-
+            ---X-
+            ---X-
+            -----
+        `)
+        let topLeftCorner = calculatorCoordinateOfTopLeft(arrayOfCoordinates)
+        expectedResult = tetris.getCoordinates(`
+            -----
+            -X---
+            -----
+            -----
+            -----
+        `)
+    })
+})
+describe("Coordinates spaced apart, is 2 elements, does not match coordinate of any of the elements", function(){
+    it("Arrays is many elements", function(){
+        arrayOfCoordinates = tetris.getCoordinates(`
+            -----
+            -----
+            ----X
+            -----
+            ---X-
+        `)
+        let topLeftCorner = calculatorCoordinateOfTopLeft(arrayOfCoordinates)
+        expectedResult = tetris.getCoordinates(`
+            X----
+            -----
+            -----
+            -----
+            -----
+        `)
+    })
+
+})
+describe("Coordinates spaced apart, is 2 elements, does not match coordinate of any of the elements", function(){
+    it("Arrays is many elements", function(){
+        arrayOfCoordinates = tetris.getCoordinates(`
+            -----
+            -----
+            ----X
+            -----
+            ---X-
+        `)
+        let topLeftCorner = calculatorCoordinateOfTopLeft(arrayOfCoordinates)
+        expectedResult = tetris.getCoordinates(`
+            X----
+            -----
+            -----
+            -----
+            -----
+        `)
+})
+
+describe("Coordinates spaced apart, is 2 elements, does not match coordinate of any of the elements", function(){
+    it("Arrays is many elements", function(){
+        arrayOfCoordinates = tetris.getCoordinates(`
+            -------
+            -------
+            -------
+            ------X
+            -------
+            
+        `)
+        let topLeftCorner = calculatorCoordinateOfTopLeft(arrayOfCoordinates)
+        expectedResult = tetris.getCoordinates(`
+            X------
+            -------
+            -------
+            -------
+            -------
+        `)
+    })
+})
+
