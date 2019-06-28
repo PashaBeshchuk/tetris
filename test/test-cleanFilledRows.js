8aad6ee742389c21fd6dd3a9c3707e1006b3c881
Pavel Beshchuk <PashaBeshchuk1990@gmail.com>
2019-06-28 20:31:33 +0400

    add new test for function cleanFiledRows

--- test/test-cleanFilledRows.js
+++ test/test-cleanFilledRows.js
@@ -1,3 +1,50 @@
 var assert = require('chai').assert;
 var tetris = require('../tetris');
-describe("Add tetromino to field", function () {})
+describe("Check situation on field", function () {
+    it("Check situation where there's no filled rows", function () {
+        let fieldSize = tetris.buildField(`
+            -----
+            -----
+            -----
+        `)
+        let field = new tetris.Field(fieldSize)
+        let resultCheckField = field.cleanFilledRows()
+        let expectedResult = tetris.buildField(`
+            -----
+            -----
+            -----
+        `)
+    })
+    it("Check situation where there's 1 filled row", function () {
+        let fieldSize = tetris.buildField(`
+            -XXX-
+            XXXXX
+            -----
+        `)
+        let field = new tetris.Field(fieldSize)
+        let resultCheckField = field.cleanFilledRows()
+        let expectedResult = tetris.buildField(`
+            -----
+            -XXX-
+            -----
+        `)
+    })
+    it("Check situation where there's more than 1 filled row", function () {
+        let fieldSize = tetris.buildField(`
+            -----
+            XXXXX
+            -XXX-
+            XXXXX
+            -----
+        `)
+        let field = new tetris.Field(fieldSize)
+        let resultCheckField = field.cleanFilledRows()
+        let expectedResult = tetris.buildField(`
+            -----
+            -----
+            -----
+            -XXX-
+            -----
+        `)
+    })
+})
