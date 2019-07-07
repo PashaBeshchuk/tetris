f09f88bf209a5c7300b67ee31b2eae8a7a3fa6d1
Pavel Beshchuk <PashaBeshchuk1990@gmail.com>
2019-07-07 14:41:32 +0400

    edit test

--- test/test-initCoordinates.js
+++ test/test-initCoordinates.js
@@ -2,66 +2,73 @@ var assert = require('chai').assert;
 var tetris = require('../tetris');
 describe("Initial coordinates tetromino", function () {
     it("Initial coordinates L tetromino", function () {
-        let typeOfTetromino = "L"
-        let field = {x: 10, y:2}
-        let coordinatesTetromino = tetris.initCoordinates(typeOfTetromino,field)
+        let typeOfTetromino = "L";
+        let field = { x: 10, y: 2 };
+        let coordinatesTetromino = tetris.initCoordinates(typeOfTetromino, field)
         let expected = tetris.getCoordinates(`
             ------X---
             ----XXX---
         `)
+        assert.deepEqual(coordinatesTetromino, expected)
     })
     it("Initial coordinates J tetromino", function () {
-        let typeOfTetromino = "J"
-        let field = {x: 10, y:2}
-        let coordinatesTetromino = tetris.initCoordinates(typeOfTetromino,field)
+        let typeOfTetromino = "J";
+        let field = { x: 10, y: 2 };
+        let coordinatesTetromino = tetris.initCoordinates(typeOfTetromino, field)
         let expected = tetris.getCoordinates(`
             ----X-----
             ----XXX---
-        `)
+        `);
+        assert.deepEqual(coordinatesTetromino, expected)
     })
     it("Initial coordinates T tetromino", function () {
-        let typeOfTetromino = "T"
-        let field = {x: 10, y:2}
-        let coordinatesTetromino = tetris.initCoordinates(typeOfTetromino,field)
+        let typeOfTetromino = "T";
+        let field = { x: 10, y: 2 };
+        let coordinatesTetromino = tetris.initCoordinates(typeOfTetromino, field)
         let expected = tetris.getCoordinates(`
             -----X----
             ----XXX---
-        `)
+        `);
+        assert.deepEqual(coordinatesTetromino, expected)
     })
     it("Initial coordinates Z tetromino", function () {
-        let typeOfTetromino = "Z"
-        let field = {x: 10, y:2}
-        let coordinatesTetromino = tetris.initCoordinates(typeOfTetromino,field)
+        let typeOfTetromino = "Z";
+        let field = { x: 10, y: 2 };
+        let coordinatesTetromino = tetris.initCoordinates(typeOfTetromino, field)
         let expected = tetris.getCoordinates(`
             ----XX----
             -----XX---
-        `)
+        `);
+        assert.deepEqual(coordinatesTetromino, expected)
     })
     it("Initial coordinates S tetromino", function () {
-        let typeOfTetromino = "S"
-        let field = {x: 10, y:2}
-        let coordinatesTetromino = tetris.initCoordinates(typeOfTetromino,field)
+        let typeOfTetromino = "S";
+        let field = { x: 10, y: 2 };
+        let coordinatesTetromino = tetris.initCoordinates(typeOfTetromino, field)
         let expected = tetris.getCoordinates(`
             -----XX---
             ----XX----
-        `)
+        `);
+        assert.deepEqual(coordinatesTetromino, expected)
     })
     it("Initial coordinates I tetromino", function () {
-        let typeOfTetromino = "I"
-        let field = {x: 10, y:2}
-        let coordinatesTetromino = tetris.initCoordinates(typeOfTetromino,field)
+        let typeOfTetromino = "I";
+        let field = { x: 10, y: 2 };
+        let coordinatesTetromino = tetris.initCoordinates(typeOfTetromino, field)
         let expected = tetris.getCoordinates(`
             ---XXXX---
             ----------
-        `)
+        `);
+        assert.deepEqual(coordinatesTetromino, expected)
     })
     it("Initial coordinates O tetromino", function () {
-        let typeOfTetromino = "O"
-        let field = {x: 10, y:2}
-        let coordinatesTetromino = tetris.initCoordinates(typeOfTetromino,field)
+        let typeOfTetromino = "O";
+        let field = { x: 10, y: 2 };
+        let coordinatesTetromino = tetris.initCoordinates(typeOfTetromino, field)
         let expected = tetris.getCoordinates(`
             ----XX----
             ----XX----
-        `)
+        `);
+        assert.deepEqual(coordinatesTetromino, expected)
     })
 })
--- test/test-startingPhaseOfTetromino.js
+++ test/test-startingPhaseOfTetromino.js
@@ -3,43 +3,43 @@ var tetris = require('../tetris');
 describe("Identification starting phase tetromino", function () {
     it("Starting phase L tetromino", function () {
         let typeOfTetromino = "L"
-        let identificationPhase = tetris.startingPhaseOfTetromino (typeOfTetromino)
+        let identificationPhase = tetris.startingPhaseOfTetromino(typeOfTetromino)
         let expected = "right"
         assert.deepEqual(identificationPhase, expected)
     })
     it("Starting phase J tetromino", function () {
         let typeOfTetromino = "J"
-        let identificationPhase = tetris.startingPhaseOfTetromino (typeOfTetromino)
+        let identificationPhase = tetris.startingPhaseOfTetromino(typeOfTetromino)
         let expected = "left"
         assert.deepEqual(identificationPhase, expected)
     })
     it("Starting phase T tetromino", function () {
         let typeOfTetromino = "T"
-        let identificationPhase = tetris.startingPhaseOfTetromino (typeOfTetromino)
+        let identificationPhase = tetris.startingPhaseOfTetromino(typeOfTetromino)
         let expected = "up"
         assert.deepEqual(identificationPhase, expected)
     })
     it("Starting phase Z tetromino", function () {
         let typeOfTetromino = "Z"
-        let identificationPhase = tetris.startingPhaseOfTetromino (typeOfTetromino)
+        let identificationPhase = tetris.startingPhaseOfTetromino(typeOfTetromino)
         let expected = "horizontal"
         assert.deepEqual(identificationPhase, expected)
     })
     it("Starting phase S tetromino", function () {
         let typeOfTetromino = "S"
-        let identificationPhase = tetris.startingPhaseOfTetromino (typeOfTetromino)
+        let identificationPhase = tetris.startingPhaseOfTetromino(typeOfTetromino)
         let expected = "horizontal"
         assert.deepEqual(identificationPhase, expected)
     })
     it("Starting phase I tetromino", function () {
         let typeOfTetromino = "I"
-        let identificationPhase = tetris.startingPhaseOfTetromino (typeOfTetromino)
+        let identificationPhase = tetris.startingPhaseOfTetromino(typeOfTetromino)
         let expected = "vertical"
         assert.deepEqual(identificationPhase, expected)
     })
     it("Starting phase O tetromino", function () {
         let typeOfTetromino = "O"
-        let identificationPhase = tetris.startingPhaseOfTetromino (typeOfTetromino)
+        let identificationPhase = tetris.startingPhaseOfTetromino(typeOfTetromino)
         let expected = "stable"
         assert.deepEqual(identificationPhase, expected)
     })
