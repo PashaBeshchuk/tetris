04cca6682182ba0e74c54cc6692d7b96c2717d2a
Pavel Beshchuk <PashaBeshchuk1990@gmail.com>
2019-09-26 14:25:31 +0400

    edit test startingPhaseOfTetromino

--- test/test-startingPhaseOfTetromino.js
+++ test/test-startingPhaseOfTetromino.js
@@ -1,45 +1,55 @@
-var assert = require('chai').assert;
-var tetris = require('../tetris');
+let assert = require('chai').assert;
+let tetris = require('../tetris');
+let sinon = require("sinon")
 describe("Identification starting phase tetromino", function () {
+    let stubForGetRandomTypeOfTetromino = sinon.stub(tetris, "getRandomTypeOfTetromino")
+    let fieldSize = { x: 10, y: 2 };
     it("Starting phase L tetromino", function () {
-        let typeOfTetromino = "L"
-        let identificationPhase = tetris.startingPhaseOfTetromino(typeOfTetromino)
+        stubForGetRandomTypeOfTetromino.returns(new tetris.TetrominoL(fieldSize))
+        let typeOfTetromino = tetris.createTetromino(fieldSize)
+        let identificationPhase = typeOfTetromino.typeOfTetromino.startingPhaseOfTetromino
         let expected = "right"
         assert.deepEqual(identificationPhase, expected)
     })
     it("Starting phase J tetromino", function () {
-        let typeOfTetromino = "J"
-        let identificationPhase = tetris.startingPhaseOfTetromino(typeOfTetromino)
+        stubForGetRandomTypeOfTetromino.returns(new tetris.TetrominoJ(fieldSize))
+        let typeOfTetromino = tetris.createTetromino(fieldSize)
+        let identificationPhase = typeOfTetromino.typeOfTetromino.startingPhaseOfTetromino
         let expected = "left"
         assert.deepEqual(identificationPhase, expected)
     })
     it("Starting phase T tetromino", function () {
-        let typeOfTetromino = "T"
-        let identificationPhase = tetris.startingPhaseOfTetromino(typeOfTetromino)
+        stubForGetRandomTypeOfTetromino.returns(new tetris.TetrominoT(fieldSize))
+        let typeOfTetromino = tetris.createTetromino(fieldSize)
+        let identificationPhase = typeOfTetromino.typeOfTetromino.startingPhaseOfTetromino
         let expected = "up"
         assert.deepEqual(identificationPhase, expected)
     })
     it("Starting phase Z tetromino", function () {
-        let typeOfTetromino = "Z"
-        let identificationPhase = tetris.startingPhaseOfTetromino(typeOfTetromino)
+        stubForGetRandomTypeOfTetromino.returns(new tetris.TetrominoZ(fieldSize))
+        let typeOfTetromino = tetris.createTetromino(fieldSize)
+        let identificationPhase = typeOfTetromino.typeOfTetromino.startingPhaseOfTetromino
         let expected = "horizontal"
         assert.deepEqual(identificationPhase, expected)
     })
     it("Starting phase S tetromino", function () {
-        let typeOfTetromino = "S"
-        let identificationPhase = tetris.startingPhaseOfTetromino(typeOfTetromino)
+        stubForGetRandomTypeOfTetromino.returns(new tetris.TetrominoS(fieldSize))
+        let typeOfTetromino = tetris.createTetromino(fieldSize)
+        let identificationPhase = typeOfTetromino.typeOfTetromino.startingPhaseOfTetromino
         let expected = "horizontal"
         assert.deepEqual(identificationPhase, expected)
     })
     it("Starting phase I tetromino", function () {
-        let typeOfTetromino = "I"
-        let identificationPhase = tetris.startingPhaseOfTetromino(typeOfTetromino)
+        stubForGetRandomTypeOfTetromino.returns(new tetris.TetrominoI(fieldSize))
+        let typeOfTetromino = tetris.createTetromino(fieldSize)
+        let identificationPhase = typeOfTetromino.typeOfTetromino.startingPhaseOfTetromino
         let expected = "horizontal"
         assert.deepEqual(identificationPhase, expected)
     })
     it("Starting phase O tetromino", function () {
-        let typeOfTetromino = "O"
-        let identificationPhase = tetris.startingPhaseOfTetromino(typeOfTetromino)
+        stubForGetRandomTypeOfTetromino.returns(new tetris.TetrominoO(fieldSize))
+        let typeOfTetromino = tetris.createTetromino(fieldSize)
+        let identificationPhase = typeOfTetromino.typeOfTetromino.startingPhaseOfTetromino
         let expected = "stable"
         assert.deepEqual(identificationPhase, expected)
     })
